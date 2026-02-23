6a901b40dd4749c48b124397768e4afcd2f9c362
--global <--global>
2026-02-23 17:00:58 -0300

    feat: Inicializa a API com Fastify e configura o script de desenvolvimento para usar variáveis de ambiente.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Teste com o typescript')
+import Fastify from 'fastify'
+const fastify = Fastify({
+  logger: true
+})
+
+fastify.get('/', () => {
+  return { message: 'Hello World' }
+})
+
+fastify.listen({ port: Number(process.env.PORT ?? 3333) }, () => {
+  console.log(`Server running on port ${process.env.PORT ?? 3333} 🚀`)
+})
