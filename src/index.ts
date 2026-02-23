6daaf30f75ab773f44cc31ff5706d8f064465966
--global <--global>
2026-02-23 17:09:13 -0300

    feat: Adiciona servidor Fastify com validação de esquema usando Zod.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,35 @@
 import Fastify from 'fastify'
-const fastify = Fastify({
-  logger: true
+import {
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider
+} from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+const app = Fastify({
+  logger: process.env.NODE_ENV === 'development' ? true : false
 })
 
-fastify.get('/', () => {
-  return { message: 'Hello World' }
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.withTypeProvider<ZodTypeProvider>().route({
+  method: 'GET',
+  url: '/zod',
+  schema: {
+    response: {
+      description: 'Success response',
+      tags: ['zod'],
+      200: z.object({
+        message: z.string()
+      })
+    }
+  },
+  handler: () => {
+    return { message: 'Hello World from ZOD Provider' }
+  }
 })
 
-fastify.listen({ port: Number(process.env.PORT ?? 3333) }, () => {
+app.listen({ port: Number(process.env.PORT ?? 3333) }, () => {
   console.log(`Server running on port ${process.env.PORT ?? 3333} 🚀`)
 })
