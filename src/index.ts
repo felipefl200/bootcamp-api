673a5419f710697096ee6bf35b939f62b736d590
--global <--global>
2026-02-23 18:20:17 -0300

    feat: Adiciona documentação da API com Fastify Swagger e configurações de desenvolvimento.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
 import Fastify from 'fastify'
 import {
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider
@@ -13,13 +16,34 @@ const app = Fastify({
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 
+await app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Bootcamp Treinos API',
+      description: 'API de treinos',
+      version: '1.0.0'
+    },
+    servers: [
+      {
+        description: 'Development server',
+        url: 'http://localhost:3333'
+      }
+    ]
+  },
+  transform: jsonSchemaTransform
+})
+
+await app.register(fastifySwaggerUi, {
+  routePrefix: '/docs'
+})
+
 app.withTypeProvider<ZodTypeProvider>().route({
   method: 'GET',
   url: '/zod',
   schema: {
+    description: 'Success response',
+    tags: ['zod'],
     response: {
-      description: 'Success response',
-      tags: ['zod'],
       200: z.object({
         message: z.string()
       })
@@ -30,6 +54,5 @@ app.withTypeProvider<ZodTypeProvider>().route({
   }
 })
 
-app.listen({ port: Number(process.env.PORT ?? 3333) }, () => {
-  console.log(`Server running on port ${process.env.PORT ?? 3333} 🚀`)
-})
+await app.listen({ port: Number(process.env.PORT ?? 3333), host: '0.0.0.0' })
+console.log(`Server running on port ${process.env.PORT ?? 3333} 🚀`)
